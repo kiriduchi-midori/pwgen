9b0712db4ce89604184e1cfc7366c09757155737
錐槌 碧 <142077067+kiriduchi-midori@users.noreply.github.com>
2024-11-22 15:43:46 +0900

    Update main.cc

--- main.cc
+++ main.cc
@@ -3,18 +3,23 @@
 #include <string>
 
 int main(int argc, char ** argv) {
-  if(argc < 2) {
-    std::cout << "arg[1]: length of password" << std::endl;
-    return EXIT_FAILURE;
-  }
   std::random_device seed;
   std::mt19937 random(seed());
-  std::string res(std::stoi(argv[1]), 0);
+  constexpr auto default_len = 128, min_len = 16;
+  uint32_t length = default_len;
+  if(argc < 2) {
+    std::cerr << "arg[1]: length of password" << std::endl;
+    length = (random() % default_len) + min_len;
+  }
+  else {
+    length = std::stoi(argv[1]);
+  }
+  std::string res(length, 0);
 
   for (int i = 0; i < res.size(); ++i) {
     res[i] = random() % 0x7f;
     while(res[i] < 0x21) {
-      res[i] = random() % 0x7f;
+        res[i] = random() % 0x7f;
     }
   }
   std::cout << res;
